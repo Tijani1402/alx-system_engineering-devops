3e31ee8b25a01eeb2ac77858880d0c92935c5166
Tijani1402 <99301758+Tijani1402@users.noreply.github.com>
2022-07-27 16:41:52 +0100

    optinal task

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[Phone_number|name:(.*?)\] \[Receiver_number|name:(.*?)\] \[flags:(.*?)\]/).join(",")
+puts ARGV[0].scan(/\[Phone_number:(.*?)\] \[Receiver_number:(.*?)\] \[flags:(.*?)\]/).join(",")
